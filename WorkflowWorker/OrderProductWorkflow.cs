adb88a460f886031599d354bc0d1bb0093a31c6c
elangelaar <ellenlangelaar@gmail.com>
2017-09-12 10:15:09 +0200

    Added WorkflowWorker to run the actual workflow, this can be used in multiple projects

---
+++ WorkflowWorker/OrderProductWorkflow.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using WorkflowWorker.OrderProductServiceReference;
+
+namespace WorkflowWorker
+{
+    public static class OrderProductWorkflow
+    {
+        /// <summary>
+        /// Start the workflow for a new order.
+        /// </summary>
+        /// <param name="order">A reference to an Order object.</param>
+        public static void RunSubmitOrder(ref Order order)
+        {
+            using (var client = new OrderProductServiceReference.ServiceClient())
+            {
+                client.SubmitOrder(ref order);
+            }
+        }
+
+        /// <summary>
+        /// Continue the workflow of an order.
+        /// </summary>
+        /// <param name="managerResponse">The managers response to an order.</param>
+        /// <returns></returns>
+        public static Order RunApproveOrder(ManagerResponse managerResponse)
+        {
+            using (var client = new OrderProductServiceReference.ServiceClient())
+            {
+                return client.ApproveOrder(managerResponse);
+            }
+        }
+
+        /// <summary>
+        /// Get a list with dummy products.
+        /// </summary>
+        /// <returns></returns>
+        private static IEnumerable<Product> GetDummyProducts()
+        {
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    Name = "Paper cup",
+                    SKU = "PC1234",
+                    Description = "Medium sized paper cup."
+                },
+                new Product
+                {
+                    Name = "Plastic Plate",
+                    SKU = "PP5678",
+                    Description = "Round white plastic plate."
+                },
+                new Product
+                {
+                    Name = "Balloon",
+                    SKU = "BA1234",
+                    Description = "Red plain balloon."
+                }
+            };
+
+            return products;
+        }
+    }
+}
